f9aa33bf57ff6f73552203d575ef7f0b8469bbdd
darkheart101 <kurokokoro@windowslive.com>
2019-07-02 20:57:38 +0300

    init

---
+++ nginx_php_mysql/app/code/index.php
@@ -0,0 +1,19 @@
+<?php
+
+ phpinfo();
+
+// $servername = "127.0.0.1";
+// $port = "33060";
+// $username = "root";
+// $password = "secret";
+
+// try {
+//     $conn = new PDO("mysql:host=$servername;port=$port;dbname=maindb", $username, $password);
+//     // set the PDO error mode to exception
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     echo "Connected successfully";
+//     }
+// catch(PDOException $e)
+//     {
+//     echo "Connection failed: " . $e->getMessage();
+//     }
