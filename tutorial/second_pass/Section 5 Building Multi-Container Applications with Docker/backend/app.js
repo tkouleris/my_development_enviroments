b14156e05ec3dac42af1c2b5b957d1b5f112b1f5
Thodoris Kouleris <kurokokoro@windowslive.com>
2026-01-09 06:37:58 +0200

    docker

--- tutorial/second_pass/Section 5 Building Multi-Container Applications with Docker/backend/app.js
+++ tutorial/second_pass/Section 5 Building Multi-Container Applications with Docker/backend/app.js
@@ -84,7 +84,8 @@ app.delete('/goals/:id', async (req, res) => {
 });
 
 mongoose.connect(
-  'mongodb://tkouleris:secret@mongodb:27017/course-goals?authSource=admin',
+  // 'mongodb://tkouleris:secret@mongodb:27017/course-goals?authSource=admin',
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
