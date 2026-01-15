9499f15a4db091d211b879915a3e509fea4c7adb
Thodoris Kouleris <kurokokoro@windowslive.com>
2026-01-15 06:21:54 +0200

    mysql

--- python_mysql/python/src/app.py
+++ python_mysql/python/src/app.py
@@ -1,9 +1,30 @@
-from flask import Flask 
+from flask import Flask, jsonify
+import MySQLdb
+import os
 
 app = Flask(__name__)
 
+DB_CONFIG = {
+    "host": os.getenv("MYSQL_HOST", "mysql"),
+    "user": os.getenv("MYSQL_USER", "myuser"),
+    "passwd": os.getenv("MYSQL_PASSWORD", "mypass"),
+    "db": os.getenv("MYSQL_DATABASE", "mydb"),
+    "port": int(os.getenv("MYSQL_PORT", 3306)),
+}
+
+def get_db_connection():
+    return MySQLdb.connect(
+        host=DB_CONFIG["host"],
+        user=DB_CONFIG["user"],
+        passwd=DB_CONFIG["passwd"],
+        db=DB_CONFIG["db"],
+        port=DB_CONFIG["port"],
+        charset="utf8mb4",
+    )
+
 @app.route("/")
 def hello_world():
+    conn = get_db_connection()
     return "Hello World!"
 
 if __name__ == "__main__":
