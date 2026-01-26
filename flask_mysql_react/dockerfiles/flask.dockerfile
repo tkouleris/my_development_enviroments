28da417c1317b2733935ca2ac19ed56257d8c37d
Thodoris Kouleris <kurokokoro@windowslive.com>
2026-01-26 06:27:08 +0200

    flask - pip

--- flask_mysql_react/dockerfiles/flask.dockerfile
+++ flask_mysql_react/dockerfiles/flask.dockerfile
@@ -8,8 +8,6 @@ RUN apt-get update && apt-get install -y \
     default-libmysqlclient-dev \
     && rm -rf /var/lib/apt/lists/*
 
-COPY src/backend/requirements.txt /app/
+# RUN pip install --no-cache-dir -r requirements.txt
 
-RUN pip install --no-cache-dir -r requirements.txt
-
-CMD ["flask", "run", "--host=0.0.0.0", "--debug"]
+CMD ["python", "-m", "flask", "run", "--host=0.0.0.0", "--debug"]
---
+++ flask_mysql_react/src/backend/app.py
@@ -0,0 +1,39 @@
+
+
+import os
+
+# from dotenv import load_dotenv
+from flask import Flask
+# from flask_bcrypt import Bcrypt
+# from flask_migrate import Migrate
+# from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+def create_app():
+
+    app = Flask(__name__)
+    
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
+    # app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')    
+    return app
+
+
+app = create_app()
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
+# bcrypt = Bcrypt(app)
+
+
+
+@app.route("/")
+def hello_world():    
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=True
+    ) 
